0ceb6cf820f13edee73691bfff604574ed49b4f9
jexpop <46821108+jexpop@users.noreply.github.com>
2026-04-06 18:18:11 +0200

    day 2

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,8 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            //const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://xumfkbx4qf.execute-api.eu-west-1.amazonaws.com/chat', {                
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
